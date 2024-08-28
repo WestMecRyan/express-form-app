e2d6b3225e6a6b6c1cb418495f09f8b489706d60
Ryan Morales <ryan.morales@west-mec.org>
2024-08-28 11:45:47 -0700

    working server

--- server/index.js
+++ server/index.js
@@ -1,20 +1,31 @@
 // require express as express
+const express = require('express');
 // require path as path
+const path = require('path');
 // require fs as fs
-
+const fs = require('fs');
 // cache the express module as 'app'
-
+const app = express();
 // cache the client directory as 'clientPath' using path.join
 const clientPath = path.join(__dirname, '..', 'client');
 // use the static middleware to serve the client folder
-
+app.use(express.static(clientPath));
 // get the '/' endpoint
 // test it with a basic 200 and end response
+app.get('/', (req, res) => {
+    res
+        .status(200)
+        .send('It works!');
+});
 // then send the index.html file
 // use error handling to catch sendFile errors
 
 // get the '/about' endpoint and serve an about page
 
 // cache a PORT to process.envPORT || 3000
+const PORT = process.env.PORT || 3000;
 // listen on the PORT server
 // log that the server is runnig
+app.listen(PORT, () => {
+    console.log(`Server is listening on PORT ${PORT}`);
+});
